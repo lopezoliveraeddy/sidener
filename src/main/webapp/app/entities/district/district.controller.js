c0a91d3a12a0c209414f09f57083fcd77af57065
Unknown <moieddomna@gmail.com>
2017-10-24 22:12:51 -0500

    Carga de Elecciones + Causales

--- src/main/webapp/app/entities/district/district.controller.js
+++ src/main/webapp/app/entities/district/district.controller.js
@@ -5,9 +5,9 @@
         .module('sidenerApp')
         .controller('DistrictController', DistrictController);
 
-    DistrictController.$inject = ['$state', 'District', 'DistrictSearch', 'ParseLinks', 'AlertService', 'paginationConstants', 'pagingParams'];
+    DistrictController.$inject = ['$state', 'District', 'DistrictSearch', 'ParseLinks', 'AlertService', 'paginationConstants', 'pagingParams', 'Election'];
 
-    function DistrictController($state, District, DistrictSearch, ParseLinks, AlertService, paginationConstants, pagingParams) {
+    function DistrictController($state, District, DistrictSearch, ParseLinks, AlertService, paginationConstants, pagingParams, Election) {
 
         var vm = this;
 
@@ -21,6 +21,8 @@
         vm.loadAll = loadAll;
         vm.searchQuery = pagingParams.search;
         vm.currentSearch = pagingParams.search;
+
+        vm.election = [];
 
         loadAll();
 
@@ -52,6 +54,24 @@
                 vm.queryCount = vm.totalItems;
                 vm.districts = data;
                 vm.page = pagingParams.page;
+                // Obtenemos datos de la Elección
+                for(var i = 0; i < data.length; i++) {
+                    loadElection(data[i].electionId, data[i].id);
+                }
+            }
+            function onError(error) {
+                AlertService.error(error.data.message);
+            }
+        }
+
+        function loadElection(electionId, id) {
+            Election.get({ id : electionId }, onSuccess, onError);
+
+            function onSuccess(data) {
+                if(angular.isUndefined(vm.election[id])) {
+                    vm.election[id] = [];
+                }
+                vm.election[id] = data;
             }
             function onError(error) {
                 AlertService.error(error.data.message);
